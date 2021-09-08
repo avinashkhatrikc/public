add4b6267e8d6d767125aa3f03dc48ba4ead003e
Avinash Khatri K C <74090907+avinashkhatrikc@users.noreply.github.com>
2021-09-08 16:10:05 +0545

    Update webserverbasedOD.ino

--- robot/webserverbasedOD.ino
+++ robot/webserverbasedOD.ino
@@ -1,15 +1,15 @@
 #include <ESP8266WiFi.h>
- 
-const char* ssid = "Avinash";
+
+const char* ssid = "NASTAI";
 const char* password = "11111111";
 
-const int trig = D3;
-const int echo = D4;
+const int trig = D4;
+const int echo = D3;
 const int in1 = D5;
 const int in2 = D6;
 const int in3 = D7;
 const int in4 = D8;
-const int ENA= D2;
+const int ENA= D0;
 const int ENB= D1;
 
 WiFiServer server(80);
@@ -55,8 +55,25 @@ long dura, dis;
 
 void loop()
 { 
-  analogWrite(ENA,85);
-  analogWrite(ENB,85);
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  
+  
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
   
@@ -68,89 +85,124 @@ void loop()
   dis = dura*0.034/2;
   Serial.print("Distance:");
   Serial.println(dis);
-  if(dis<25)
-    {
-      digitalWrite(in1, LOW); 
-      digitalWrite(in2, HIGH); 
-      digitalWrite(in3, HIGH); 
-      digitalWrite(in4, LOW);
-       delay(200);
-       Serial.println("Back");
-      digitalWrite(in1, LOW); 
-      digitalWrite(in3, LOW); 
-      digitalWrite(in2, HIGH); 
-      digitalWrite(in4, LOW);
-      Serial.println("Right");
-      delay(500);
-    }
-    
-  else
-    {
-            // Check if a client has connected
-           WiFiClient client = server.available();
-           if (!client) {
-             return;
-           }
-
-           // Wait until the client sends some data
-           Serial.println("new client");
-           while(!client.available()){
-             delay(1);
-           }
-
-           // Read the first line of the request
-           String request = client.readStringUntil('\r');
-           Serial.println(request);
-           client.flush();
-
                // Match the request
-           if (request.indexOf("/forward=1") != -1)  {  
-            digitalWrite(in1, HIGH); //Start first motor
-            digitalWrite(in2, LOW);
-            digitalWrite(in4, HIGH); //Start second motor
-            digitalWrite(in3, LOW);
-            Serial.println("Motor On and Running in forward Direction");
-            client.println("Motor On and Running in forward Direction");
+           if (request.indexOf("/forward") != -1)  {  
+                                if(dis<25)
+                        {
+                          analogWrite(ENA,900);
+                          analogWrite(ENB, 900);
+                          digitalWrite(in1, HIGH); 
+                          digitalWrite(in2, LOW); 
+                          digitalWrite(in3, LOW); 
+                          digitalWrite(in4, HIGH);
+                           delay(1000);
+                          //stop
+                          digitalWrite(in1, LOW); 
+                          digitalWrite(in3, LOW); 
+                          digitalWrite(in2, LOW); 
+                          digitalWrite(in4, LOW);
+                          
+                          
+                        }
+
+            analogWrite(ENA,900);
+            analogWrite(ENB, 900);          
+            digitalWrite(in1, LOW); //Start first motor
+            digitalWrite(in2, HIGH);
+            
+            digitalWrite(in3, HIGH);
+            digitalWrite(in4, LOW); //Start second motor
+            delay(600);
+            digitalWrite(in1, LOW); 
+                          digitalWrite(in3, LOW); 
+                          digitalWrite(in2, LOW); 
+                          digitalWrite(in4, LOW);
+
+            
           }
           
-          if (request.indexOf("/stop=1") != -1)  {  
+          if (request.indexOf("/stop") != -1)  {  
+            analogWrite(ENA,0);
+            analogWrite(ENB, 0);
             digitalWrite(in1, LOW); //Stop first motor
             digitalWrite(in2, LOW);
           
             digitalWrite(in3, LOW); //Stop second motor
             digitalWrite(in4, LOW);
-            Serial.println("Motor is Stop");
-            client.println("Motor is Stop");
           }
           
-          if (request.indexOf("/backward=1") != -1)  {
+          if (request.indexOf("/backward") != -1)  {
+
+          analogWrite(ENA,900);
+          analogWrite(ENB, 900);
+          digitalWrite(in1, HIGH); 
+          digitalWrite(in2, LOW); 
+          digitalWrite(in3, LOW); 
+          digitalWrite(in4, HIGH);
+
+          delay(1000);
+                          //stop
+          digitalWrite(in1, LOW); 
+          digitalWrite(in3, LOW); 
+          digitalWrite(in2, LOW); 
+          digitalWrite(in4, LOW);
+    
+          }
+
+          if (request.indexOf("/left") != -1)  {
+          analogWrite(ENA,900);
+          analogWrite(ENB, 900);
+          digitalWrite(in1, HIGH); 
+          digitalWrite(in2, LOW); 
+          digitalWrite(in3, HIGH); 
+          digitalWrite(in4, LOW);
+          delay(1000);
+                          //stop
+          digitalWrite(in1, LOW); 
+          digitalWrite(in3, LOW); 
+          digitalWrite(in2, LOW); 
+          digitalWrite(in4, LOW);
+          
+        
+          }
+
+          if (request.indexOf("/right") != -1)  {
+
+          analogWrite(ENA,1020);
+          analogWrite(ENB, 1024);
             
           digitalWrite(in1, LOW); 
           digitalWrite(in2, HIGH); 
-          digitalWrite(in3, HIGH); 
+          digitalWrite(in3, LOW); 
+          digitalWrite(in4, HIGH);
+          delay(1000);
+                          //stop
+          digitalWrite(in1, LOW); 
+          digitalWrite(in3, LOW); 
+          digitalWrite(in2, LOW); 
           digitalWrite(in4, LOW);
           
-            Serial.println("Motor On and Running in backward Direction");
-            client.println("Motor On and Running in backward Direction");
+       
           }
            
    
-          // Return the response
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println(""); //  do not forget this one
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<h1 align=center>Nast AI Delivery Robot</h1>");
-          client.println("<h2 align=center>WiFi Based Obstacle detection robot</h2>");
-          client.println("<br><br>");
-          client.println("<p align=center><a href=\"/forward=1\"\"><button><h1>  Forward  </h1></button></a></p><br/>");
-          client.println("<p align=center><a href=\"/stop=1\"\" ><button><h1>   Stop    </h1></button></a></p><br/>");
-          client.println("<p align=center><a href=\"/backward=1\"\" ><button> <h1>  Reverse  </h1></button></a></p><br/>");
-          client.println("</html>");
-          delay(1);
-          Serial.println("Client disonnected");
-          Serial.println("");
-    }  
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<h1 align=center>Nast AI Delivery Robot</h1>");
+  client.println("<h2 align=center>WiFi Based Obstacle detection robot</h2>");
+  client.println("<br><br>");
+  client.println("<p align=center><a href=\"/forward\"\"><button><h1>  Forward  </h1></button></a></p><br/>");
+  client.println("<p align=center><a href=\"/stop\" ><button><h1>   Stop    </h1></button></a></p><br/>");
+  client.println("<p align=center><a href=\"/backward\"\" ><button> <h1>  Reverse  </h1></button></a></p><br/>");
+  client.println("<p align=center><a href=\"/right\"\" ><button><h1>   Right    </h1></button></a></p><br/>");
+  client.println("<p align=center><a href=\"/left\"\" ><button> <h1>  Left  </h1></button></a></p><br/>");
+  client.println("</html>");
+  delay(1);
+  Serial.print("Client disonnected");
+  Serial.print(""); 
   delay(0);
 }
